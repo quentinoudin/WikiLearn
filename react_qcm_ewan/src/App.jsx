2b8f3ab9ecff4ba11bb1109f7a671485f45b98d3
EwanROUX <ewanroux47@gmail.com>
2026-03-12 13:16:17 +0100

    feat: navbar complete avec logo livre et trophée étoile personnalisé

---
+++ react_qcm_ewan/src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        {/* SECTION GAUCHE : LOGO */}
+        <a href="/" className="logo-link">
+          <div className="logo-icon-box">
+            <svg className="book-svg" viewBox="0 0 40 30">
+              <path d="M20 25c-5 2-15 2-15-2V8c0-4 10-4 15-2" />
+              <path d="M20 25c5 2 15 2 15-2V8c0-4-10-4-15-2" />
+              <path d="M5 23c0 3 15 6 30 0" opacity="0.8" />
+              <line x1="20" y1="6" x2="20" y2="25" opacity="0.5" />
+            </svg>
+          </div>
+          <div className="logo-text">
+            Wikipedia <span>Learning</span>
+          </div>
+        </a>
+
+        {/* SECTION CENTRALE : MENU */}
+        <nav className="nav-menu">
+          <a href="#" className="nav-link">
+            Parcours
+          </a>
+          <a href="#" className="nav-link">
+            Leçon
+          </a>
+          <a href="#" className="nav-link">
+            Quizz
+          </a>
+        </nav>
+
+        {/* SECTION DROITE : CLASSEMENT & PROFIL */}
+        <div className="nav-right">
+          <a href="#classement" className="classement-link">
+            <svg className="trophy-svg" viewBox="0 0 32 32">
+              <path d="M28 6H24V4a2 2 0 0 0-2-2H10a2 2 0 0 0-2 2v2H4a2 2 0 0 0-2 2v2c0 1.1.9 2 2 2h4.1a6 6 0 0 0 5.9 5v4h-2a2 2 0 0 0-2 2v2h12v-2a2 2 0 0 0-2-2h-2v-4a6 6 0 0 0 5.9-5H28c1.1 0 2-.9 2-2V8a2 2 0 0 0-2-2zM4 10V8h4v2H4zm24 0h-4V8h4v2z" />{" "}
+              <path
+                d="M16 5l1.1 3.2h3.4l-2.7 2.1 1.1 3.2-2.7-2.1-2.7 2.1 1.1-3.2-2.7-2.1h3.4z"
+                fill="#FFFFFF"
+              />{" "}
+            </svg>
+            <span className="classement-text">Classement</span>
+          </a>
+          <button className="profile-btn">
+            <span className="user-icon">👤</span>
+            <span className="profile-text">Profil</span>
+          </button>
+        </div>
+      </header>
+
+      <main className="content">
+      </main>
+    </div>
+  );
+}
+
+export default App;
