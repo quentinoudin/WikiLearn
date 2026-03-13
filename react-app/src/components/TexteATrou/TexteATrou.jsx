d57c40944afebc41a0833a0e6092282cfc094912
Chipsoupe <belletpaul999@gmail.com>
2026-03-13 11:25:44 +0100

    Ajout des fichiers exemples dans les components

---
+++ react-app/src/components/TexteATrou/TexteATrou.jsx
@@ -0,0 +1,3 @@
+export default function TexteATrou() {
+  return <section>Texte à trou component</section>;
+}
---
+++ react-app/src/components/VraiFaux/VraiFaux.jsx
@@ -0,0 +1,3 @@
+export default function VraiFaux() {
+  return <section>Vrai/Faux component</section>;
+}
